b49bb6821d9b35c6fde3b5963bf012ce8ceeeb55
fabizle <fabi.estrada@yahoo.com>
2023-12-03 22:26:18 -0600

    HW 6 Submission

--- 06-Python-APIs/Starter_Code/starter_code/api_keys.py
+++ 06-Python-APIs/Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "bf9bb295949758a02e396ac1beb1ca12"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "acaa97151007443fa62e0e5eb29c0d6b"
